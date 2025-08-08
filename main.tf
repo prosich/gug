ac91064293c1ded1b9d41278da006c7fd60aef1e
prosich <prosich@gmail>
2025-08-08 10:45:31 +0200

    Creacion de VM gratis en gcloud.

---
+++ main.tf
@@ -0,0 +1,61 @@
+# ============================================================================
+# Cargar Google Cloud provider para Terraform
+# ============================================================================
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# ============================================================================
+# Indicar ID de proyecto Google Cloud.
+# ============================================================================
+# Previamente se debe haber hecho:
+# - Crear proyecto-gratis-de-paco en GC console GUI.
+# - Desde shell:
+#   gcloud services enable compute.googleapis.com --project="prgratis-paco"
+
+provider "google" {
+  project = "prgratis-paco"
+}
+
+# ============================================================================
+# Crear nuestra maquina llamada "gug" 
+# ============================================================================
+# Para que sea gratuita debe cumplir:
+#   - tipo "e2-micro"
+#   - limitado a ciertas zonas, como "us-west1-c"
+#   - disco max. 30GB de tipo "pd-standard"
+# Creamos un usuario llamado "suser" y se le añade la clave publica SSH local.
+
+resource "google_compute_instance" "vmgratis" {
+  name = "gug"
+  machine_type = "e2-micro"
+  zone = "us-west1-c"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      type = "pd-standard"
+      size = 20
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+  metadata = {
+    ssh-keys = "suser:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+# ============================================================================
+# Mostar dir. IP asignada a la maquina. 
+# ============================================================================
+output "vm_external_ip" {
+  value = google_compute_instance.vmgratis.network_interface[0].access_config[0].nat_ip
+}
+
